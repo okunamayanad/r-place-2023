9903e5a53ae529f59e7c86583fcfaaca278b9f75
okunamayanad <108594296+okunamayanad@users.noreply.github.com>
2023-07-21 06:59:05 +0300

    Add the script for generating the image

---
+++ index.js
@@ -0,0 +1,22 @@
+var Jimp = require('jimp');
+
+
+Jimp.read('place.png', (err, img) => {
+    if (err) throw err;
+    let output = img.clone();
+    output.scale(3);
+    
+    output.scan(0, 0, output.bitmap.width, output.bitmap.height, function (x, y, idx) {
+        // set alpha to 0
+        output.bitmap.data[idx + 3] = 0;
+    });
+
+    img.scan(0, 0, output.bitmap.width, output.bitmap.height, function (x, y, idx) {
+        var outx = x * 2, outy = y * 2;
+        var color = img.getPixelColor(x, y);
+        
+        output.setPixelColor(color, outx, outy);
+    });
+
+    output.write('output.png');
+});
