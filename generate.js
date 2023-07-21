6256117e0440e6e9ff5d8473679affff0d6c219c
okunamayanad <108594296+okunamayanad@users.noreply.github.com>
2023-07-21 08:01:02 +0300

    Update generate.js
    Moved one pixel right and one pixel down so that pixels will be in the center of the bigger pixels

--- generate.js
+++ generate.js
@@ -4,16 +4,16 @@ Jimp.read('place.png', (err, img) => {
     if (err) throw err;
     let output = img.clone();
     output.scale(3);
-    
+
     output.scan(0, 0, output.bitmap.width, output.bitmap.height, function (x, y, idx) {
         // set alpha to 0
         output.bitmap.data[idx + 3] = 0;
     });
 
     img.scan(0, 0, output.bitmap.width, output.bitmap.height, function (x, y, idx) {
-        var outx = x * 2, outy = y * 2;
+        var outx = x * 2 + 1, outy = y * 2 + 1;
         var color = img.getPixelColor(x, y);
-        
+
         output.setPixelColor(color, outx, outy);
     });
 
