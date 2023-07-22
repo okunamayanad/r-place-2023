26a2bf5e4db0d6404a449dd201f90aa9738129d0
okunamayanad <baran@okunamayanad.com>
2023-07-23 00:19:45 +0300

    feat: :rocket: Added a script for jahrein

--- generate.js
+++ generate.js
@@ -1,21 +1,26 @@
 var Jimp = require('jimp');
 
-Jimp.read('place.png', (err, img) => {
-    if (err) throw err;
-    let output = img.clone();
-    output.scale(3);
-
-    output.scan(0, 0, output.bitmap.width, output.bitmap.height, function (x, y, idx) {
-        // set alpha to 0
-        output.bitmap.data[idx + 3] = 0;
+function generate(imagefile, outputfile) {
+    Jimp.read(imagefile, (err, img) => {
+        if (err) throw err;
+        let output = img.clone();
+        output.scale(3);
+    
+        output.scan(0, 0, output.bitmap.width, output.bitmap.height, function (x, y, idx) {
+            // set alpha to 0
+            output.bitmap.data[idx + 3] = 0;
+        });
+    
+        img.scan(0, 0, output.bitmap.width, output.bitmap.height, function (x, y, idx) {
+            var outx = x * 3 + 1, outy = y * 3 + 1;
+            var color = img.getPixelColor(x, y);
+    
+            output.setPixelColor(color, outx, outy);
+        });
+    
+        output.write(outputfile);
     });
+}
 
-    img.scan(0, 0, output.bitmap.width, output.bitmap.height, function (x, y, idx) {
-        var outx = x * 3 + 1, outy = y * 3 + 1;
-        var color = img.getPixelColor(x, y);
-
-        output.setPixelColor(color, outx, outy);
-    });
-
-    output.write('output.png');
-});
+generate('place.png', 'output.png');
+generate('jahrein.png', 'jahrein-output.png')
---
+++ jahrein.2023.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         RPlace 2023
+// @namespace    https://okunamayanad.com/
+// @version      1.0
+// @description  Discord: @okunamayand, Github: https://github.com/okunamayanad/r-place-2023
+// @author       okunamayanad
+// @match        https://garlic-bread.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// @license MIT
+// @downloadURL        https://github.com/okunamayanad/r-place-2023/raw/main/jahrein.2023.user.js
+// @updateURL        https://github.com/okunamayanad/r-place-2023/raw/main/jahrein.2023.user.js 
+// ==/UserScript==
+console.log("1");
+if (window.top !== window.self) {
+    console.log("2");
+    // console.log("3");
+    // Load the image
+    const image = document.createElement("img");
+    var d = new Date();
+    image.src = "https://raw.githubusercontent.com/okunamayanad/r-place-2023/main/jahrein-output.png?" + d.getTime();
+    image.onload = () => {
+        image.style = `position: absolute; left: 0; top: 0; width: ${image.width / 3}px; height: ${image.height / 3}px; image-rendering: pixelated; z-index: 1`;
+    };
+
+    // Add the image as overlay
+    const camera = document.querySelector("garlic-bread-embed").shadowRoot.querySelector("garlic-bread-camera");
+    const canvas = camera.querySelector("garlic-bread-canvas");
+    const container = canvas.shadowRoot.querySelector('.container')
+    container.appendChild(image);
+
+    console.log("container", container);
+
+    // Add a style to put a hole in the pixel preview (to see the current or desired color)
+    const waitForPreview = setInterval(() => {
+        const preview = camera.querySelector("garlic-bread-pixel-preview");
+        if (preview) {
+            clearInterval(waitForPreview);
+            const style = document.createElement('style')
+            style.innerHTML = '.pixel { clip-path: polygon(-20% -20%, -20% 120%, 37% 120%, 37% 37%, 62% 37%, 62% 62%, 37% 62%, 37% 120%, 120% 120%, 120% -20%); }'
+            preview.shadowRoot.appendChild(style);
+        }
+    }, 100);
+}
